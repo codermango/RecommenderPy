d7e6ef6a57a96baaec21689805568534d2b913d3
codermango <codermango@gmail.com>
2015-03-23 17:14:27 +0100

    start build php page, change layout

---
+++ demo/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+        <script type="text/javascript" src="js/base.js"></script>
+    </head>
+    <body onload="startLoad()">
+        <div id="main">
+            <div id="liked-movies">
+                <ul id="liked-movies-list" class="drop-buckets">
+                    <?php
+                        $fileArr = array();
+                        $file = fopen("../recommender/mark_liked_movie_id.txt", "r");
+                        while (!feof($file)) {
+                            $id = fgets($file);
+                            $fileArr[] = $id;
+                        }
+                        fclose($file);
+
+                        foreach ($fileArr as $id) {
+                            echo "<li>$id</li>";
+                        }
+                    ?>
+
+                </ul>
+            </div>
+            <div id="recommended-movies">
+                <ul id="recommended-movies-list">
+                    <?php
+                        //system("python ../recommender/recommender.py");
+                        $result = exec("python ../demo/test.py");
+                        echo "==". $result. "===";
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
--- demo/js/base.js
+++ demo/js/base.js
@@ -7,65 +7,73 @@ function startLoad() {
 
 // 以下为页面加载时，给相应标签加上drag属性和事件
 function setDragTrueAndAddEvent() {
-	
-	var recommendedLiList = document.getElementById("recommended-movies-list").getElementsByTagName("li");
-	var likedMovieLiList = document.getElementById("liked-movies-list").getElementsByTagName("li");
 
-	for (var i = 0; i < recommendedLiList.length; i++) {
+    var recommendedImgList = document.querySelectorAll("#recommended-movies-list li img");
+    var likedMovieLiList = document.querySelectorAll("#liked-movies-list li");
+
+
+	for (var i = 0; i < recommendedImgList.length; i++) {
 		// 1. 给recommended-movies里地li加上drappable="true"
-		recommendedLiList[i].draggable = "true";
+		recommendedImgList[i].draggable = "true";
 
 		// 2. 给recommende-movies里地li加上ondragstart函数
-		recommendedLiList[i].addEventListener("dragstart", function() {handleDragStart(event);});
+		recommendedImgList[i].addEventListener("dragstart", function() {handleDragStart(event);}, false);
 	}
 
 	for (var i = 0; i < likedMovieLiList.length; i++) {
 		// 3. 给drop-buckets里地li加上ondragenter和ondrop函数
-		likedMovieLiList[i].addEventListener("dragenter", function() {handleDragEnter(event);});
-		likedMovieLiList[i].addEventListener("dragleave", function() {handleDragLeave(event);});
-
-		likedMovieLiList[i].addEventListener("drop", function() {handleDrop(event);});
-		likedMovieLiList[i].addEventListener("dragover", function() {handleDragOver(event);}, false);
+		likedMovieLiList[i].addEventListener("dragenter", function() {handleDragEnter(event);}, false);
+        likedMovieLiList[i].addEventListener("dragover", function() {handleDragOver(event);}, false);
+		likedMovieLiList[i].addEventListener("dragleave", function() {handleDragLeave(event);}, false);
+		likedMovieLiList[i].addEventListener("drop", function() {handleDrop(event);}, false);
+        likedMovieLiList[i].addEventListener("dragend", function() {handleDragEnd(event);}, false);
+		
+        console.log(likedMovieLiList[i].innerHTML + 'aaa');
 	}
 }
 
+
+function handleDragStart(e) {
+    e.dataTransfer.setData("imgPath", e.target.src);
+    //console.log(e.target.src);
+}
+
+
 function handleDragOver(e) {
 	e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    return false;
 }
 
 function handleDragEnter(e) {
-	e.preventDefault();
 	e.target.classList.add("over");
 	//e.target.style.backgroundColor = "white";
 }
 
 
-function handleDragStart(e) {
-	dragSource = e.target;
-	e.dataTransfer.setData("imgPath", e.target.src);
-	//console.log(e.target.src);
-}
-
-
 function handleDragLeave(e) {
-
 	e.target.classList.remove("over");
 }
 
 function handleDrop(e) {
-	e.preventDefault();
+	e.stopPropagation();
 	targetEle = e.target;
 	targetEle.classList.remove("over");
 	var childNodes = targetEle.childNodes;
-	console.log(childNodes.length);
+	//console.log(childNodes.length);
 
 	if (childNodes.length == 0) {
 		var imgPath = e.dataTransfer.getData("imgPath");
 		targetEle.innerHTML = "<img src='" + imgPath + "' >";
-		console.log(targetEle.innerHTML);
+		//console.log(targetEle.innerHTML);
 	}
 
+    return false;
+}
 
+
+function handleDragEnd(e) {
+    e.target.classList.remove("over");
 }
 
 
